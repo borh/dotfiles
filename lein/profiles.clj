efc9f24c3b33872c2eed4e009fe7a025b892713f
Bor Hodošček <bor.hodoscek@gmail.com>
2013-01-17 23:47:16 +0900

    lein config

---
+++ lein/profiles.clj
@@ -0,0 +1,13 @@
+{:user {:plugins [[lein-difftest "1.3.8"]
+                  [lein-marginalia "0.7.1"]
+                  [lein-pprint "1.1.1"]
+                  [lein-swank "1.4.4"]
+                  [jonase/kibit "0.0.4"]
+                  [lein-midje "2.0.1"]
+                  [lein-midje-lazytest "0.1.0"]
+                  [lazytest "1.2.3"]
+                  [lein-expectations "0.0.7"]
+                  [lein-autoexpect "0.2.2"]
+                  [lein-pedantic "0.0.5"]
+		  [lein-newnew "0.3.4"]
+                  [lein-ritz "0.5.0"]]}}
