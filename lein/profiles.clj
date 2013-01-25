19710df566d283e7378ef74ebd88428bc74fc4e3
Bor Hodošček <bor.hodoscek@gmail.com>
2013-01-25 16:27:19 +0900

    new lein plugins

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -8,6 +8,8 @@
                   [lazytest "1.2.3"]
                   [lein-expectations "0.0.7"]
                   [lein-autoexpect "0.2.2"]
+		  [lein-outdated "1.0.0"]
                   [lein-pedantic "0.0.5"]
 		  [lein-newnew "0.3.4"]
+		  [lein-exec "0.2.1"]
                   [lein-ritz "0.5.0"]]}}
