9546d4db425a2360ea07e77cc5dfb02e18d9bf57
Bor Hodošček <bor.hodoscek@gmail.com>
2013-01-25 16:26:00 +0900

    reenable mozc

--- emacs.d/bor/mozc-init.el
+++ emacs.d/bor/mozc-init.el
@@ -1,7 +1,7 @@
-;(require 'mozc)
-;(set-language-environment "Japanese")
-;(setq default-input-method "japanese-mozc")
+(require 'mozc)
+(set-language-environment "Japanese")
+(setq default-input-method "japanese-mozc")
 ;; need to test this; or maybe fall back to \C-z for whole system
 ;(global-set-key (kbd "<Zenkaku_Hankaku>") 'toggle-input-method)
 ;(global-set-key (kbd "<Menu>") 'toggle-input-method)
-;(global-set-key (kbd "C-z") 'toggle-input-method)
+(global-set-key (kbd "C-z") 'toggle-input-method)
