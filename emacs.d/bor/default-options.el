98f9ed9ef64d978f31149ff650b1aa6ea915f6b9
Bor Hodošček <bor.hodoscek@gmail.com>
2013-02-23 12:37:36 +0900

    only disable scroll-bars and menus when in gui mode

--- emacs.d/bor/default-options.el
+++ emacs.d/bor/default-options.el
@@ -2,6 +2,10 @@
 (remove-hook 'prog-mode-hook 'esk-turn-on-hl-line-mode)
 (remove-hook 'prog-mode-hook 'auto-fill-mode)
 (remove-hook 'text-mode-hook 'auto-fill-mode)
-(dolist (f '(menu-bar-mode scroll-bar-mode tool-bar-mode)) (funcall f -1))
+(if (display-graphic-p)
+    (progn
+      (menu-bar-mode -1)
+      (tool-bar-mode -1)
+      (scroll-bar-mode -1)))
 (add-hook 'text-mode-hook
           '(lambda() (set-fill-column 100))) ; better, longer default
