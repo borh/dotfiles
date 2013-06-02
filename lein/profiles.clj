d9ff5b254b0e4021102e45d180f4f2df50c6e685
Bor Hodošček <bor.hodoscek@gmail.com>
2013-06-02 20:15:39 +0900

    cleanup lein deps and fix pprint mapping

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -18,21 +18,15 @@
                   [lein-clojars "0.9.1"]
                   [lein-marginalia "0.7.1"]
                   [lein-pprint "1.1.1"]
-                  [lein-swank "1.4.4"]
                   [lein-kibit "0.0.8" :exclusions [org.clojure/clojure]]
                   [lein-bikeshed "0.1.3"]
-                  [lein-midje "3.0.1"]
-                  [lein-midje-lazytest "0.1.0" :exclusions [org.clojure/clojure]]
                   [lazytest "1.2.3"]
-                  [lein-expectations "0.0.8"]
-                  [lein-autoexpect "0.2.5"]
                   [lein-outdated "1.0.0"]
                   [lein-exec "0.3.0"]
                   [lein-bin "0.3.2"]
                   [org.clojars.strongh/lein-init-script "1.3.1"]
-                  [http-kit/lein-template "1.0.0-SNAPSHOT"]
                   [lein-ritz "0.7.0"]]
-        :repl-options {:custom-eval (require '[bbloom.fipp.edn :refer (pprint) :rename {pprint fipp}])
+        :repl-options {:custom-eval (require '[fipp.edn :refer (pprint) :rename {pprint fipp}])
                        :nrepl-middleware
                        [ritz.nrepl.middleware.javadoc/wrap-javadoc
                         ritz.nrepl.middleware.simple-complete/wrap-simple-complete
