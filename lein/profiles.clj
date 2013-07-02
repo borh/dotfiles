488373758ac0ff73385c9ae97cccb9fd493f244b
Bor Hodošček <bor.hodoscek@gmail.com>
2013-07-02 11:37:22 +0900

    update/clean deps

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,19 +1,16 @@
 {:user {:deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                           :creds :gpg}]]
-        :dependencies [[spyscope "0.1.3" :exclusions [org.clojure/clojure]]
-                       [print-foo "0.3.2" :exclusions [org.clojure/clojure]]
-                       [fipp "0.3.0-SNAPSHOT"]
+        :dependencies [[spyscope "0.1.3" :exclusions [clj-time]]
+                       [print-foo "0.3.7"]
+                       [fipp "0.4.0"]
                        [ritz/ritz-nrepl-middleware "0.7.0"]
-                       [clojure-complete "0.2.2"]
+                       [clojure-complete "0.2.3"]
                        [table "0.4.0"]
                        [slamhound "1.3.3"]
-                       [javert "0.2.0-SNAPSHOT"]]
+                       [nrepl-inspect "0.3.0"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        :injections [[(require 'spyscope.core)]
-                     ;;[(use 'clojure.pprint)]
-                     ;;[(use 'print.foo)]
-                     ;;[(use '[bbloom.fipp.edn])]
-                     ]
+        :injections [[(try (require 'spyscope.core)
+                           (catch RuntimeException e))]]
         :plugins [[lein-difftest "2.0.0"]
                   [lein-clojars "0.9.1"]
                   [lein-marginalia "0.7.1"]
@@ -21,13 +18,13 @@
                   [lein-kibit "0.0.8" :exclusions [org.clojure/clojure]]
                   [lein-bikeshed "0.1.3"]
                   [lazytest "1.2.3"]
-                  [lein-outdated "1.0.0"]
+                  [lein-ancient "0.4.0"]
                   [lein-exec "0.3.0"]
-                  [lein-bin "0.3.2"]
+                  [lein-bin "0.3.4"]
                   [org.clojars.strongh/lein-init-script "1.3.1"]
                   [lein-ritz "0.7.0"]]
         :repl-options {:custom-eval (require '[fipp.edn :refer (pprint) :rename {pprint fipp}])
                        :nrepl-middleware
-                       [ritz.nrepl.middleware.javadoc/wrap-javadoc
-                        ritz.nrepl.middleware.simple-complete/wrap-simple-complete
-                        inspector.middleware/wrap-inspect]}}}
+                       [inspector.middleware/wrap-inspect
+                        ritz.nrepl.middleware.javadoc/wrap-javadoc
+                        ritz.nrepl.middleware.apropos/wrap-apropos]}}}
