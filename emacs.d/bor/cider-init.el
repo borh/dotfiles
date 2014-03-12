f530351ac5f5ddad482c275056af7b3bb300dae4
Bor Hodošček <bor.hodoscek@gmail.com>
2014-03-12 21:18:21 +0900

    cider fixes

--- emacs.d/bor/cider-init.el
+++ emacs.d/bor/cider-init.el
@@ -32,9 +32,15 @@
 
      (require 'auto-complete)
 
+     (require 'ac-cider-compliment)
+     (add-hook 'cider-mode-hook 'ac-cider-compliment-setup)
+     (add-hook 'cider-mode-hook 'ac-flyspell-workaround)
+     (eval-after-load "auto-complete"
+       '(add-to-list 'ac-modes cider-mode))
      (defun set-auto-complete-as-completion-at-point-function ()
        (setq completion-at-point-functions '(auto-complete)))
      (add-hook 'auto-complete-mode-hook 'set-auto-complete-as-completion-at-point-function)
+     (add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
 
      (add-hook 'cider-repl-mode-hook 'set-auto-complete-as-completion-at-point-function)
      (add-hook 'cider-mode-hook 'set-auto-complete-as-completion-at-point-function)
@@ -44,6 +50,13 @@
      ;; Help with alignment
      (require 'align-cljlet)
 
-     (defun repl-reset ()
-       (interactive)
-       (cider-interactive-eval "(user/reset)"))))
+     ;; (defun repl-reset ()
+     ;;   (interactive)
+     ;;   (save-some-buffers)
+     ;;   (with-current-buffer (cider-current-repl-buffer)
+     ;;     (goto-char (point-max))
+     ;;     (insert "(user/reset)")
+     ;;     (cider-repl-return)))
+     ;;
+     ;; (global-set-key (kbd "C-c C-l") cider-repl-reset)
+     ))
