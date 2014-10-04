8c687951de54cd27565ce6ddcd4d1395e57d56e4
Bor Hodošček <bor.hodoscek@gmail.com>
2014-10-04 10:28:29 +0100

    God-mode tweaks

--- emacs.d/bor/god-mode-init.el
+++ emacs.d/bor/god-mode-init.el
@@ -1,3 +1,12 @@
+(defun c/god-mode-update-cursor ()
+  (let ((limited-colors-p (> 257 (length (defined-colors)))))
+    (cond (god-local-mode (progn
+                            (set-face-background 'mode-line (if limited-colors-p "pink" "#ff5fff"))
+                            (set-face-background 'mode-line-inactive (if limited-colors-p "pink" "#ff5fff"))))
+          (t (progn
+               (set-face-background 'mode-line (if limited-colors-p "black" "#0a2832"))
+               (set-face-background 'mode-line-inactive (if limited-colors-p "black" "#0a2832")))))))
+
 (eval-after-load "god-mode"
   '(progn
      (global-set-key (kbd "<escape>") 'god-local-mode)
@@ -7,4 +16,6 @@
      (global-set-key (kbd "C-x C-2") 'split-window-below)
      (global-set-key (kbd "C-x C-3") 'split-window-right)
      (global-set-key (kbd "C-x C-0") 'delete-window)
-     (add-to-list 'god-exempt-major-modes 'dired-mode)))
+     (add-to-list 'god-exempt-major-modes 'dired-mode)
+     (add-hook 'god-mode-enabled-hook 'c/god-mode-update-cursor)
+     (add-hook 'god-mode-disabled-hook 'c/god-mode-update-cursor)))
