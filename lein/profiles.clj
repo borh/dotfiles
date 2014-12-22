bc5a381ff52339c03f720738c3fe7d773ad7866b
Bor Hodošček <bor.hodoscek@gmail.com>
2014-12-22 03:46:16 +0000

    updated lein plugin deps

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,16 +2,16 @@
                                           :creds :gpg}]]
         :dependencies [[leiningen "2.5.0" :exclusions [org.codehaus.plexus/plexus-utils org.clojure/tools.macro com.fasterxml.jackson.core/jackson-core]]
                        ;;[im.chit/vinyasa "0.2.0"]
-                       [spyscope "0.1.4"]
-                       [io.aviso/pretty "0.1.12"]
+                       [spyscope "0.1.5"]
+                       [io.aviso/pretty "0.1.13"]
                        [com.aphyr/prism "0.1.2"]
-                       [fipp "0.4.3"]
-                       [compliment "0.1.4"]
+                       [fipp "0.5.1"]
+                       [compliment "0.2.0"]
                        [table "0.4.0"]
                        [com.ambrosebs/dynalint "0.1.3"]
                        [criterium "0.4.3"]
                        [slamhound "1.5.5"]
-                       [cider/cider-nrepl "0.8.0-SNAPSHOT"]]
+                       [cider/cider-nrepl "0.8.2"]]
         :aliases {"slamhound" ["run" "-m" "slam.hound"]}
         :plugins [[lein-difftest "2.0.0"]
                   [com.aphyr/prism "0.1.2"]
@@ -20,9 +20,9 @@
                   [lein-marginalia "0.8.0"]
                   [lein-pprint "1.1.2"]
                   [lein-kibit "0.0.8" :exclusions [org.clojure/clojure]]
-                  [jonase/eastwood "0.1.4" :exclusions [org.clojure/clojure]]
+                  [jonase/eastwood "0.2.0" :exclusions [org.clojure/clojure]]
                   [lein-dynalint "0.1.4"]
-                  [lein-bikeshed "0.1.8"]
+                  [lein-bikeshed "0.2.0"]
                   [lazytest "1.2.3"]
                   [lein-ancient "0.5.5"]
                   [lein-datomic "0.2.0"]
@@ -30,13 +30,6 @@
                   [lein-bin "0.3.4"]
                   [lein-localrepo "0.5.3"]
                   [org.clojars.strongh/lein-init-script "1.3.1"]
-                  [cider/cider-nrepl "0.8.0-SNAPSHOT"]]
-        :datomic {:install-location "../datomic-free-0.9.4556"}
-        :repl-options {:nrepl-middleware
-                       [;;io.aviso.nrepl/pretty-middleware
-                        cider.nrepl.middleware.classpath/wrap-classpath
-                        cider.nrepl.middleware.complete/wrap-complete
-                        cider.nrepl.middleware.info/wrap-info
-                        cider.nrepl.middleware.inspect/wrap-inspect
-                        cider.nrepl.middleware.stacktrace/wrap-stacktrace
-                        cider.nrepl.middleware.trace/wrap-trace]}}}
+                  [cider/cider-nrepl "0.8.2"]
+                  [refactor-nrepl "0.2.2"]]
+        :datomic {:install-location "../datomic-free-0.9.4556"}}}
