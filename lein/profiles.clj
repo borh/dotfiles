737039476ba6ee5e6f5bfd112fc9ae757f380191
Bor Hodošček <bor.hodoscek@gmail.com>
2015-01-02 17:01:47 +0900

    cleaned up profile

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,35 +1,34 @@
 {:user {:deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                           :creds :gpg}]]
-        :dependencies [[leiningen "2.5.0" :exclusions [org.codehaus.plexus/plexus-utils org.clojure/tools.macro com.fasterxml.jackson.core/jackson-core]]
-                       ;;[im.chit/vinyasa "0.2.0"]
-                       [spyscope "0.1.5"]
-                       [io.aviso/pretty "0.1.13"]
-                       [com.aphyr/prism "0.1.2"]
+        :dependencies [[io.aviso/pretty "0.1.13"]
                        [fipp "0.5.1"]
                        [compliment "0.2.0"]
                        [table "0.4.0"]
-                       [com.ambrosebs/dynalint "0.1.3"]
                        [criterium "0.4.3"]
-                       [slamhound "1.5.5"]
-                       [cider/cider-nrepl "0.8.2"]]
-        :aliases {"slamhound" ["run" "-m" "slam.hound"]}
-        :plugins [[lein-difftest "2.0.0"]
-                  [com.aphyr/prism "0.1.2"]
-                  [lein-typed "0.3.5"]
+                       [cider/cider-nrepl "0.8.2" :exclusions [org.clojure/java.classpath]]]
+        :plugins [[lein-typed "0.3.5"]
                   [lein-clojars "0.9.1"]
-                  [lein-marginalia "0.8.0"]
-                  [lein-pprint "1.1.2"]
                   [lein-kibit "0.0.8" :exclusions [org.clojure/clojure]]
-                  [jonase/eastwood "0.2.0" :exclusions [org.clojure/clojure]]
-                  [lein-dynalint "0.1.4"]
+                  [jonase/eastwood "0.2.1" :exclusions [org.clojure/clojure]]
                   [lein-bikeshed "0.2.0"]
-                  [lazytest "1.2.3"]
-                  [lein-ancient "0.5.5"]
-                  [lein-datomic "0.2.0"]
-                  [lein-exec "0.3.4"]
-                  [lein-bin "0.3.4"]
+                  [lein-ancient "0.5.5" :exclusions [org.clojure/tools.reader org.clojure/clojure]]
                   [lein-localrepo "0.5.3"]
-                  [org.clojars.strongh/lein-init-script "1.3.1"]
-                  [cider/cider-nrepl "0.8.2"]
-                  [refactor-nrepl "0.2.2"]]
-        :datomic {:install-location "../datomic-free-0.9.4556"}}}
+                  [cider/cider-nrepl "0.8.2"]]
+        :datomic {:install-location "../datomic-free-0.9.4556"}
+        :repl-options {:nrepl-middleware
+                       [;;io.aviso.nrepl/pretty-middleware
+                        cider.nrepl.middleware.apropos/wrap-apropos
+                        cider.nrepl.middleware.classpath/wrap-classpath
+                        cider.nrepl.middleware.complete/wrap-complete
+                        cider.nrepl.middleware.info/wrap-info
+                        cider.nrepl.middleware.inspect/wrap-inspect
+                        cider.nrepl.middleware.macroexpand/wrap-macroexpand
+                        cider.nrepl.middleware.ns/wrap-ns
+                        cider.nrepl.middleware.resource/wrap-resource
+                        cider.nrepl.middleware.stacktrace/wrap-stacktrace
+                        cider.nrepl.middleware.test/wrap-test
+                        cider.nrepl.middleware.trace/wrap-trace
+                        cider.nrepl.middleware.undef/wrap-undef
+                        cider.nrepl.middleware.classpath/wrap-classpath
+                        cider.nrepl.middleware.complete/wrap-complete
+                        cider.nrepl.middleware.info/wrap-info]}}}
