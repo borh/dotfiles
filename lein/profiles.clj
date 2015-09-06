c59968766e646e11beea374b5e35ad95322e03b8
Bor Hodošček <bor.hodoscek@gmail.com>
2015-09-07 08:54:50 +0900

    cleaned up updated deps

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,11 +1,9 @@
 {:user {:deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                           :creds :gpg}]]
-        :dependencies [;;[io.aviso/pretty "0.1.18"]
-                       [fipp "0.6.2"]
-                       ;;[compliment "0.2.4"]
+        :dependencies [[fipp "0.6.2"]
                        [table "0.5.0"]
                        [criterium "0.4.3"]
-                       [cider/cider-nrepl "0.9.1" :exclusions [org.clojure/java.classpath]]]
+                       [cider/cider-nrepl "0.10.0-SNAPSHOT" :exclusions [org.clojure/java.classpath]]]
         :plugins [[lein-typed "0.3.5"]
                   [lein-clojars "0.9.1"]
                   [lein-cljfmt "0.3.0"]
@@ -16,10 +14,10 @@
                   [lein-bikeshed "0.2.0"]
                   [lein-ancient "0.6.7" :exclusions [org.clojure/tools.reader org.clojure/clojure]]
                   [lein-localrepo "0.5.3"]
-                  [cider/cider-nrepl "0.9.1"]]
+                  [refactor-nrepl "1.2.0-SNAPSHOT"]
+                  [cider/cider-nrepl "0.10.0-SNAPSHOT"]]
         :repl-options {:nrepl-middleware
-                       [;;io.aviso.nrepl/pretty-middleware
-                        cider.nrepl.middleware.apropos/wrap-apropos
+                       [cider.nrepl.middleware.apropos/wrap-apropos
                         cider.nrepl.middleware.classpath/wrap-classpath
                         cider.nrepl.middleware.complete/wrap-complete
                         cider.nrepl.middleware.info/wrap-info
