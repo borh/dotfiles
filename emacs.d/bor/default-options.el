73e7d7cc5ac13d8f7a8b17edbb36edaee834afd6
Bor Hodošček <bor.hodoscek@gmail.com>
2015-12-30 18:06:58 +0900

    experimenting with different scroll options

--- emacs.d/bor/default-options.el
+++ emacs.d/bor/default-options.el
@@ -12,10 +12,11 @@
       (menu-bar-mode -1)
       (tool-bar-mode -1)
       (scroll-bar-mode -1)
-      ;;(toggle-fullscreen)
-      ))
+      (toggle-fullscreen)))
 
 (add-hook 'text-mode-hook
           '(lambda() (set-fill-column 100))) ; better, longer default
 
-(require 'god-mode)
+(setq scroll-conservatively 101) ;; move minimum when cursor exits view, instead of recentering
+(setq mouse-wheel-scroll-amount '(1)) ;; mouse scroll moves 1 line at a time, instead of 5 lines
+(setq mouse-wheel-progressive-speed nil) ;; on a long mouse scroll keep scrolling by 1 line
