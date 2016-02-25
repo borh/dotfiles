e89db10eacd6049b281d723f16e788b57ce41c5f
Bor Hodošček <bor.hodoscek@gmail.com>
2016-02-25 21:28:50 +0900

    changed to n3-mode for now

--- emacs.d/bor/ttl-init.el
+++ emacs.d/bor/ttl-init.el
@@ -1,9 +1,21 @@
-(autoload 'ttl-mode "ttl-mode" "Major mode for OWL or Turtle files" t)
-(add-hook 'ttl-mode-hook    ; Turn on font lock when in ttl mode
-          'turn-on-font-lock)
-(setq auto-mode-alist
-      (append
-       (list
-        '("\\.n3" . ttl-mode)
-        '("\\.ttl" . ttl-mode))
-       auto-mode-alist))
+;; (use-package
+;;   ttl-mode
+;;   :config
+;;   (add-hook 'ttl-mode-hook    ; Turn on font lock when in ttl mode
+;;             'turn-on-font-lock)
+;;   (setq auto-mode-alist
+;;         (append
+;;          (list
+;;           '("\\.n3" . ttl-mode)
+;;           '("\\.ttl" . ttl-mode))
+;;          auto-mode-alist)))
+
+(use-package
+  n3-mode
+  :config
+  (setq auto-mode-alist
+        (append
+         (list
+          '("\\.n3" . n3-mode)
+          '("\\.owl" . n3-mode))
+         auto-mode-alist)))
