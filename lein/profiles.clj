0e61177a301bf32675c47f9429a13063f8517d9f
Bor Hodošček <bor.hodoscek@gmail.com>
2016-03-05 09:59:46 +0900

    updated deps

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -7,19 +7,19 @@
                        [criterium "0.4.4"]
                        [acyclic/squiggly-clojure "0.1.5"]
                        [org.clojure/tools.nrepl "0.2.12"]
-                       [cider/cider-nrepl "0.11.0-SNAPSHOT"]]
+                       [cider/cider-nrepl "0.11.0"]]
         :plugins [[lein-typed "0.3.5"]
                   [lein-clojars "0.9.1"]
-                  [lein-cljfmt "0.3.0"]
+                  [lein-cljfmt "0.4.1"]
                   ;;[venantius/yagni "0.1.4" :exclusions [org.clojure/clojure org.clojure/tools.logging]]
                   [lein-collisions "0.1.4" :exclusions [org.clojure/clojure]]
                   [lein-kibit "0.1.2" :exclusions [org.clojure/clojure]]
                   [jonase/eastwood "0.2.3" :exclusions [org.clojure/clojure]]
-                  [lein-bikeshed "0.2.0"]
+                  [lein-bikeshed "0.3.0"]
                   [lein-ancient "0.6.8" :exclusions [org.clojure/clojure]]
                   [lein-localrepo "0.5.3"]
                   [refactor-nrepl "2.2.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.11.0-SNAPSHOT"]]
+                  [cider/cider-nrepl "0.11.0"]]
         :repl-options {:nrepl-middleware
                        [cider.nrepl.middleware.apropos/wrap-apropos
                         cider.nrepl.middleware.classpath/wrap-classpath
