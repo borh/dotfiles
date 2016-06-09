ec4e898d6c71e569d680fb0d262ad8ae326d2f71
Bor Hodoscek <bor@Bors-MacBook-Pro.local>
2016-06-09 14:12:21 +0900

    improved lang env settings

--- config.fish
+++ config.fish
@@ -57,9 +57,17 @@ alias tmux "tmux -2"
 alias y "youtube-dl --prefer-free-formats --no-mtime -t"
 alias pstree "pstree -U"
 
-set -x LC_ALL en_US.utf8
-set -x LANGUAGE en_US.utf8
-set -x LANG en_US.utf8
+set -x LANG en_US.utf-8
+set -x LANGUAGE en_US.utf-8
+set -x LC_CTYPE en_US.UTF-8
+set -x LC_COLLATE en_US.utf-8
+set -x LC_CTYPE en_US.utf-8
+set -x LC_MESSAGES en_US.utf-8
+set -x LC_MONETARY en_US.utf-8
+set -x LC_NUMERIC en_US.utf-8
+set -x LC_TIME en_US.utf-8
+set -x LC_ALL en_US.utf-8
+
 set -x TZ 'Asia/Tokyo'
 
 # IME (mozc)
