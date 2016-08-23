49ddb898ca6dae2b2f94ad548c5ddc2a7ae2e4be
Bor Hodošček <bor.hodoscek@gmail.com>
2016-08-23 21:55:31 +0900

    use-packageify ess

--- emacs.d/bor/ess-init.el
+++ emacs.d/bor/ess-init.el
@@ -1,1 +1,11 @@
-(require 'ess-site)
+(use-package
+  ess
+  :config
+  (require 'ess-site)
+  (defun mdw-mode ()
+    (ess-noweb-mode)
+    (setq ess-noweb-default-code-mode 'ess-julia-mode)
+    (setq ess-noweb-doc-mode 'markdown-mode))
+
+  (setq auto-mode-alist (append (list (cons "\\.mdw$" 'mdw-mode))
+                                auto-mode-alist)))
