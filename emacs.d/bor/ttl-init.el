311b96f8a718630270eae22ae0d2235e96e0ab37
Bor Hodošček <bor.hodoscek@gmail.com>
2016-11-01 20:00:15 +0900

    re-enable ttl config

--- emacs.d/bor/ttl-init.el
+++ emacs.d/bor/ttl-init.el
@@ -1,14 +1,14 @@
-;; (use-package
-;;   ttl-mode
-;;   :config
-;;   (add-hook 'ttl-mode-hook    ; Turn on font lock when in ttl mode
-;;             'turn-on-font-lock)
-;;   (setq auto-mode-alist
-;;         (append
-;;          (list
-;;           '("\\.n3" . ttl-mode)
-;;           '("\\.ttl" . ttl-mode))
-;;          auto-mode-alist)))
+(use-package
+  ttl-mode
+  :config
+  (add-hook 'ttl-mode-hook    ; Turn on font lock when in ttl mode
+            'turn-on-font-lock)
+  (setq auto-mode-alist
+        (append
+         (list
+          ;;'("\\.n3" . ttl-mode)
+          '("\\.ttl" . ttl-mode))
+         auto-mode-alist)))
 
 (use-package
   n3-mode
