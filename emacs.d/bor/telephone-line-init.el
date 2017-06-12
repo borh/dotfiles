00fb49a4a49072a4ba173d63f912d650ae321d56
Bor Hodošček <bor.hodoscek@gmail.com>
2017-06-12 11:11:35 +0900

    converted telephone-line to use-package

--- emacs.d/bor/telephone-line-init.el
+++ emacs.d/bor/telephone-line-init.el
@@ -1,2 +1,10 @@
-(require 'telephone-line)
-(telephone-line-mode 1)
+(use-package
+  telephone-line
+  :config
+  (setq telephone-line-primary-left-separator 'telephone-line-cubed-left
+        telephone-line-secondary-left-separator 'telephone-line-cubed-hollow-left
+        telephone-line-primary-right-separator 'telephone-line-cubed-right
+        telephone-line-secondary-right-separator 'telephone-line-cubed-hollow-right)
+  (setq telephone-line-height 24
+        telephone-line-evil-use-short-tag t)
+  (telephone-line-mode 1))
