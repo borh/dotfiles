6a4b0775f80b9e2fbddb39de1b2fc164b097a13e
Bor Hodošček <bor.hodoscek@gmail.com>
2017-06-12 10:15:21 +0900

    simplified lein profile deps

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -2,19 +2,8 @@
                                           :creds :gpg}]]
         #_:mirrors #_{#"clojars" {:name "clojars mirror"
                                   :url "https://clojars-mirror.tcrawley.org/repo/"}}
-        :dependencies [[fipp "0.6.8"]
-                       [table "0.5.0"]
-                       [criterium "0.4.4"]
-                       [org.clojure/tools.nrepl "0.2.12"]
-                       [cider/cider-nrepl "0.15.0-SNAPSHOT"]]
-        :plugins [[lein-clojars "0.9.1"]
-                  [lein-cljfmt "0.5.6"]
-                  [lein-collisions "0.1.4" :exclusions [org.clojure/clojure]]
-                  [lein-bikeshed "0.4.1"]
-                  [lein-ancient "0.6.10" :exclusions [org.clojure/clojure]]
-                  [lein-localrepo "0.5.3"]
-                  [refactor-nrepl "2.3.0-SNAPSHOT"]
-                  [cider/cider-nrepl "0.15.0-SNAPSHOT"]]
+        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
+                       [cider/cider-nrepl "0.14.0"]]
         :repl-options {:nrepl-middleware
                        [cider.nrepl.middleware.apropos/wrap-apropos
                         cider.nrepl.middleware.classpath/wrap-classpath
