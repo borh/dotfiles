0778a8830ff125b864897b6f845516e6ca33cba4
Bor Hodošček <bor.hodoscek@gmail.com>
2017-06-13 14:12:23 +0900

    added missing ansible vault function

--- emacs.d/bor/ansible-init.el
+++ emacs.d/bor/ansible-init.el
@@ -2,5 +2,9 @@
   ansible
   :config
   (use-package ansible-vault
+    :config
+    (defun ansible-vault-mode-maybe ()
+      (when (ansible-vault--is-vault-file)
+        (ansible-vault-mode 1)))
     :init
     (add-hook 'yaml-mode-hook 'ansible-vault-mode-maybe)))
