74f9ca9f4420f59fb4a000683ccfd9acc3dbd9cf
Bor Hodošček <bor.hodoscek@gmail.com>
2017-07-25 12:44:56 +0900

    keepassxc crash workaround and mu index settings

--- fish/config.fish
+++ fish/config.fish
@@ -6,7 +6,8 @@ set -x PAGER less
 set -x SSH_ASKPASS /usr/bin/ksshaskpass
 set -x XDG_RUNTIME_DIR /run/user/(id -u)
 set -x XDG_CURRENT_DESKTOP kde
-
+set -x QT_QPA_PLATFORMTHEME qt5ct # Seems to crash keepassxc when set to KDE
+set -x XAPIAN_CJK_NGRAM 1
 set -x XKB_DEFAULT_OPTIONS ctrl:nocaps
 setxkbmap -option ctrl:nocaps
 
