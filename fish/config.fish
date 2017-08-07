f7b0f482a46b2d892412c6325d5bee352bc95617
Bor Hodošček <bor.hodoscek@gmail.com>
2017-08-07 10:17:17 +0900

    added tensorflow/cuda env flags

--- fish/config.fish
+++ fish/config.fish
@@ -72,6 +72,27 @@ set -x VBOX_USB usbfs
 set -x CFLAGS "-O2 -march=native -pipe"
 set -x CXXFLAGS "-O2 -march=native -pipe"
 set -x CHOST "x86_64-pc-linux-gnu"
+set -x CC_OPT_FLAGS "-march=native"
+
+# TensorFlow
+
+set -x TF_NEED_JEMALLOC 1
+set -x TF_NEED_GCP 0
+set -x TF_NEED_HDFS 0
+set -x TF_ENABLE_XLA 1
+set -x TF_NEED_VERBS 0
+set -x TF_NEED_OPENCL 0
+set -x TF_NEED_MKL 1
+set -x TF_NEED_MPI 0
+set -x TF_NEED_CUDA 1
+set -x GCC_HOST_COMPILER_PATH /usr/bin/gcc-5
+set -x TF_CUDA_CLANG 0
+set -x CLANG_CUDA_COMPILER_PATH /usr/bin/clang
+set -x CUDA_TOOLKIT_PATH /opt/cuda
+set -x TF_CUDA_VERSION (eval $CUDA_TOOLKIT_PATH/bin/nvcc --version | sed -n 's/^.*release \(.*\),.*/\1/p')
+set -x CUDNN_INSTALL_PATH /opt/cuda
+set -x TF_CUDNN_VERSION (sed -n 's/^#define CUDNN_MAJOR\s*\(.*\).*/\1/p' $CUDNN_INSTALL_PATH/include/cudnn.h)
+set -x TF_CUDA_COMPUTE_CAPABILITIES '5.2,6.1'
 
 # go
 set -x GOOS linux
