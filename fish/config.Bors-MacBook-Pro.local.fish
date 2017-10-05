55fc5ff3935028813883f54e620fa8a83fd0b5a1
Bor Hodošček <bor.hodoscek@gmail.com>
2017-10-05 14:48:12 +0900

    absolute path

--- fish/config.Bors-MacBook-Pro.local.fish
+++ fish/config.Bors-MacBook-Pro.local.fish
@@ -1,6 +1,6 @@
 set -x PATH $PATH /usr/local/opt/go/libexec/bin $HOME/.cargo/bin
 
-set -x HOMEBREW_GITHUB_API_TOKEN (cat github-token)
+set -x HOMEBREW_GITHUB_API_TOKEN (cat ~/.github-token)
 set -x LESSCHARSET utf-8
 
 set -x CHOST "x86_64-apple-darwin17.0.0"
