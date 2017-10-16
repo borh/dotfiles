9679c44fb9c40089ded2eb9bcef6b8fd893f7e5d
Bor Hodošček <bor.hodoscek@gmail.com>
2017-10-17 07:28:28 +0900

    converted to use-package

--- emacs.d/bor/company-mode-init.el
+++ emacs.d/bor/company-mode-init.el
@@ -1,2 +1,24 @@
-(require 'company)
-(add-hook 'after-init-hook 'global-company-mode)
+;; (use-package company
+;;   :config
+;;
+;;   (use-package
+;;     company-jedi
+;;     :config
+;;     (defun my/python-mode-hook ()
+;;       (add-to-list 'company-backends 'company-jedi))
+;;
+;;     (add-hook 'python-mode-hook 'my/python-mode-hook))
+;;
+;;   (use-package
+;;     company-auctex
+;;     :config
+;;     (company-auctex-init))
+;;
+;;   ;; (use-package
+;;   ;;   company-math
+;;   ;;   :config
+;;   ;;   (add-to-list 'company-backends 'company-math-symbols-unicode))
+;;
+;;   (setq company-tooltip-align-annotations t)
+;;
+;;   (add-hook 'after-init-hook 'global-company-mode))
