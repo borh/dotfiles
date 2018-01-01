896988b39f48875e62b80c91114a4f5ac2008fb7
Bor Hodošček <bor.hodoscek@gmail.com>
2018-01-01 19:35:03 +0900

    updated lein/boot

--- boot/profile.boot
+++ boot/profile.boot
@@ -1,13 +1,22 @@
+(import java.io.File)
+
+;; Get credentials from a GPG encrypted file.
+(configure-repositories!
+ (let [creds-file (File. (boot.App/bootdir) "credentials.gpg")
+       creds-data (gpg-decrypt creds-file :as :edn)]
+   (fn [{:keys [url] :as repo-map}]
+     (merge repo-map (creds-data url)))))
+
+;;(task-options! push {:repo "https://repo.clojars.org/"})
+
 (deftask cider "CIDER profile"
   []
   (require 'boot.repl)
   (swap! @(resolve 'boot.repl/*default-dependencies*)
          concat '[[criterium "0.4.4" :exclusions [org.clojure/clojure] :scope "test"]
-                  [fipp "0.6.8" :scope "test"]
-
-                  [org.clojure/tools.nrepl "0.2.13"]
-                  [cider/cider-nrepl "0.15.1-SNAPSHOT"]
-                  [refactor-nrepl "2.4.0-SNAPSHOT"]])
+                  [org.clojure/tools.nrepl "0.2.13" :scope "test"]
+                  [cider/cider-nrepl "0.16.0" :scope "test"]
+                  [refactor-nrepl "2.4.0-SNAPSHOT" :scope "test"]])
   (swap! @(resolve 'boot.repl/*default-middleware*)
          concat '[cider.nrepl/cider-middleware
                   refactor-nrepl.middleware/wrap-refactor])
--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,27 +1,3 @@
 {:user {:deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                           :creds :gpg}]]
-        #_:mirrors #_{#"clojars" {:name "clojars mirror"
-                                  :url "https://clojars-mirror.tcrawley.org/repo/"}}
-        :dependencies [[org.clojure/tools.nrepl "0.2.12"]
-                       [cider/cider-nrepl "0.14.0"]]
-        :plugins [[lein-localrepo "0.5.3"]]
-        :repl-options {:nrepl-middleware
-                       [cider.nrepl.middleware.apropos/wrap-apropos
-                        cider.nrepl.middleware.classpath/wrap-classpath
-                        cider.nrepl.middleware.complete/wrap-complete
-                        cider.nrepl.middleware.debug/wrap-debug
-                        cider.nrepl.middleware.format/wrap-format
-                        cider.nrepl.middleware.info/wrap-info
-                        cider.nrepl.middleware.inspect/wrap-inspect
-                        cider.nrepl.middleware.macroexpand/wrap-macroexpand
-                        cider.nrepl.middleware.ns/wrap-ns
-                        cider.nrepl.middleware.pprint/wrap-pprint
-                        cider.nrepl.middleware.pprint/wrap-pprint-fn
-                        cider.nrepl.middleware.refresh/wrap-refresh
-                        cider.nrepl.middleware.resource/wrap-resource
-                        cider.nrepl.middleware.stacktrace/wrap-stacktrace
-                        cider.nrepl.middleware.test/wrap-test
-                        cider.nrepl.middleware.trace/wrap-trace
-                        cider.nrepl.middleware.out/wrap-out
-                        cider.nrepl.middleware.undef/wrap-undef
-                        cider.nrepl.middleware.version/wrap-version]}}}
+        :dependencies [[org.clojure/clojure "1.9.0"]]}}
