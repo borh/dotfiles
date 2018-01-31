4cf32e4de992527f93aba88b0b82db0999f8c975
Bor Hodošček <bor.hodoscek@gmail.com>
2018-01-31 21:32:19 +0900

    added unicode math completion for julia

--- emacs.d/bor/julia-init.el
+++ emacs.d/bor/julia-init.el
@@ -5,4 +5,16 @@
   :config
   (flycheck-julia-setup)
   (add-to-list 'flycheck-global-modes 'julia-mode)
-  (add-to-list 'flycheck-global-modes 'ess-julia-mode))
+  (add-to-list 'flycheck-global-modes 'ess-julia-mode)
+  (defun julia-completion-at-point ()
+    (let ((bnds (bounds-of-thing-at-point 'symbol)))
+      (when bnds
+        (cond
+         ;; complete latex symbol when current symbol is prefixed
+         ;; by '\'
+         ((eq (char-before (car bnds)) ?\\)
+          (list (1- (car bnds)) (cdr bnds) julia-latexsubs
+                :annotation-function
+                #'(lambda (arg)
+                    (gethash arg julia-latexsubs ""))))))))
+  (add-hook 'completion-at-point-functions 'julia-completion-at-point nil t))
