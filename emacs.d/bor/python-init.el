0b263b72cafe5f90958ad6610eff578307bee13b
Bor Hodošček <bor.hodoscek@gmail.com>
2018-02-05 10:22:21 +0900

    added pipenv emacs config

--- emacs.d/bor/python-init.el
+++ emacs.d/bor/python-init.el
@@ -4,3 +4,12 @@
   (elpy-enable)
   (setq python-shell-interpreter "ipython3"
         python-shell-interpreter-args "--simple-prompt --pprint"))
+
+(use-package pipenv
+  :ensure nil
+  :init
+  (setq
+   pipenv-projectile-after-switch-function
+   #'pipenv-projectile-after-switch-extended)
+  :config
+  (add-hook 'python-mode-hook #'pipenv-mode))
