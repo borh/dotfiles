2bbfbc9246d82e3d0e3b22ef79b36fc9584ed08d
Bor Hodošček <bor.hodoscek@gmail.com>
2018-07-06 13:14:01 +0900

    added cider back to lein

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,3 +1,5 @@
 {:user {:deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                           :creds :gpg}]]
-        :dependencies [[org.clojure/clojure "1.9.0"]]}}
+        :dependencies [[org.clojure/clojure "1.10.0-alpha4"]]
+        :plugins [[lein-ancient "0.6.15"]
+                  [cider/cider-nrepl "0.18.0-SNAPSHOT"]]}}
