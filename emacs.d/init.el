685800b859e556a1afba2dfc97886ca3d9a19c8c
Bor Hodošček <dev@bor.space>
2018-09-03 08:40:34 +0900

    fixed melpa url

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -3,7 +3,7 @@
 (add-to-list 'package-archives '("original"  . "http://tromey.com/elpa/") t)
 (add-to-list 'package-archives '("gnu"       . "http://elpa.gnu.org/packages/") t)
 (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-(add-to-list 'package-archives '("melpa"     . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa"     . "http://melpa.org/packages/") t)
 (add-to-list 'package-archives '("org"       . "http://orgmode.org/elpa/") t)
 (package-initialize)
 (when (not package-archive-contents)
