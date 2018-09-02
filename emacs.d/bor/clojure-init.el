096c061700fc5a0184bcb02d360e349a1fd3d3bb
Bor Hodošček <dev@bor.space>
2018-09-03 08:38:14 +0900

    add yas to clojure mode

--- emacs.d/bor/clojure-init.el
+++ emacs.d/bor/clojure-init.el
@@ -56,6 +56,7 @@
   (add-hook 'clojure-mode-hook
             (lambda ()
               (clj-refactor-mode 1)
+              (yas-minor-mode 1)
               (cljr-add-keybindings-with-prefix "C-c C-m"))))
 
 ;; (use-package re-jump) ;; Not on MELPA yet.
