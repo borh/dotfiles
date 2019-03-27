011f774bf4b6833241988d287137e990ac1553c5
Bor Hodošček <dev@bor.space>
2019-03-27 19:58:06 +0900

    updated lein config

--- lein/profiles.clj
+++ lein/profiles.clj
@@ -1,5 +1,5 @@
 {:user {:deploy-repositories [["clojars" {:url "https://clojars.org/repo"
                                           :creds :gpg}]]
-        :dependencies [[org.clojure/clojure "1.10.0-alpha4"]]
+        :dependencies [[org.clojure/clojure "1.10.0"]]
         :plugins [[lein-ancient "0.6.15"]
-                  [cider/cider-nrepl "0.18.0-SNAPSHOT"]]}}
+                  [cider/cider-nrepl "0.21.1"]]}}
