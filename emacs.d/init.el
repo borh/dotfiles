a4e46cacdb1f176280d14d8bb224f490993baf11
Bor Hodošček <dev@bor.space>
2019-05-24 12:39:37 +0900

    refreshed emacs package selection

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -10,105 +10,14 @@
   (package-refresh-contents))
 
 (eval-when-compile
-  (require 'use-package))
+    (require 'use-package))
+(require 'diminish)                ;; if you use :diminish
 (require 'bind-key)                ;; if you use any :bind variant
+
+(use-package gnu-elpa-keyring-update)
 
 ;; Global to download and load all packages defined with use-package.
 (setq use-package-always-ensure t)
-
-;; Add in your own as you wish:
-(defvar
-  my-packages
-  '(auto-complete exec-path-from-shell s
-                  use-package
-                  ;; clojure related
-                  clojure-mode
-                  rainbow-delimiters rainbow-mode rainbow-identifiers
-                  align-cljlet
-                  latest-clojars flycheck-clojure
-                  cider ac-cider
-                  clj-refactor
-                  ;; color themes
-                  color-theme color-theme-sanityinc-tomorrow
-                  color-theme-sanityinc-solarized
-                  material-theme
-                  ;; general interface improvements
-                  projectile
-                  telephone-line
-                  ;;helm helm-themes ac-helm helm-projectile helm-R
-                  ;;ido-ubiquitous smex ; needed by starter-kit
-                  swiper counsel
-                  company
-                  adaptive-wrap
-                  iy-go-to-char popup typopunct expand-region
-                  smooth-scrolling tidy typing writegood-mode wc-mode wtf flyspell-lazy
-                  save-visited-files thesaurus smart-tab smart-tabs-mode undo-tree
-                  annoying-arrows-mode volatile-highlights browse-kill-ring
-                  litable sublimity parscope pangu-spacing smartparens
-                  diff-hl god-mode
-                  dash dash-functional
-                  color-identifiers-mode
-                  langtool
-                  hungry-delete
-                  ;; language related
-                  ess ess-R-data-view go-mode go-autocomplete js3-mode latex-pretty-symbols
-                  ipython python python-mode python-pep8 ein jedi
-                  ac-octave
-                  cperl-mode dart-mode
-                  coffee-mode less-css-mode scss-mode
-                  yaml-mode haskell-mode
-                  markdown-mode
-                  markdown-mode+ pandoc-mode
-                  puppet-mode
-                  apache-mode
-                  nginx-mode
-                  ssh-config-mode
-                  graphviz-dot-mode
-                  omn-mode
-                  dockerfile-mode
-                  systemd
-                  rust-mode flymake-rust flycheck-rust
-                  sql-indent format-sql
-                  ;;ansible ;;-> yasnippets error
-                  ;;nxml-mode
-                  html5-schema
-                  ;; flymake
-                  flymake-coffee flymake-css flymake-sass flymake-jshint
-                  flymake-sass
-                  flymake-perlcritic
-                  flymake-shell
-                  flymake-go
-                  pep8 flymake-python-pyflakes
-                  flycheck
-                  flycheck-status-emoji
-                  flycheck-pos-tip
-                  flyspell-lazy
-                  ;; jabber
-                  jabber
-                  ;; git
-                  magit
-                  ;; org/latex
-                  org
-                  org-table-comment org-beautify-theme org-bullets
-                  org-gcal org-mime ox-pandoc org-tracktable
-                  ox-impress-js ox-reveal
-                  org-dashboard org-multiple-keymap org-alert org-wc
-                  cl-generic ;; needed for org-store-link to function for mu4e
-                  htmlize
-                  ac-math
-                  ac-mozc
-                  ;; yasnippet
-                  ;;yasnippet
-                  nterm
-                  ;; other
-                  mmm-mode
-                  fountain-mode
-                  )
-  "A list of packages to ensure are installed at launch.")
-
-(dolist (p my-packages)
-  (when (not (package-installed-p p))
-    (package-install p)))
 
 ;; Fix emacs running under the fish shell
 (require 's)
