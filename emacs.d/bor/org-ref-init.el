29bd8ff2106e08b4d66e0cbfafd625b4a0ca6528
Bor Hodošček <dev@bor.space>
2019-05-24 12:41:30 +0900

    disabled org-ref

--- emacs.d/bor/org-ref-init.el
+++ emacs.d/bor/org-ref-init.el
@@ -1,20 +1,20 @@
-(use-package
-  org-ref
-  :bind
-  (("<f10>" . org-ref-open-bibtex-notes)
-   ("<f11>" . org-ref-open-bibtex-pdf)
-   ("<f12>" . org-ref-open-in-browse))
-  :config
-  (setq reftex-default-bibliography '("~/Dropbox/Org/Bibliography/bibliography.bib"))
-
-  ;; see org-ref for use of these variables
-  (setq org-ref-bibliography-notes "~/Dropbox/Org/Bibliography/Bibliography-Notes.org"
-        org-ref-default-bibliography '("~/Dropbox/Org/Bibliography/bibliography.bib")
-        org-ref-pdf-directory "~/Dropbox/Research/")
-
-  (setq helm-bibtex-bibliography "~/Dropbox/Org/Bibliography/bibliography.bib")
-  (setq helm-bibtex-library-path "~/Dropbox/Research/")
-
-  (setq helm-bibtex-pdf-open-function 'org-open-file)
-
-  (setq helm-bibtex-notes-path "~/Dropbox/Org/Bibliography/helm-bibtex-notes"))
+;; (use-package
+;;   org-ref
+;;   :bind
+;;   (("<f10>" . org-ref-open-bibtex-notes)
+;;    ("<f11>" . org-ref-open-bibtex-pdf)
+;;    ("<f12>" . org-ref-open-in-browse))
+;;   :config
+;;   (setq reftex-default-bibliography '("~/Cloud/Org/Bibliography/bibliography.bib"))
+;; 
+;;   ;; see org-ref for use of these variables
+;;   (setq org-ref-bibliography-notes "~/Cloud/Org/Bibliography/Bibliography-Notes.org"
+;;         org-ref-default-bibliography '("~/Cloud/Org/Bibliography/bibliography.bib")
+;;         org-ref-pdf-directory "~/Cloud/Research/")
+;; 
+;;   (setq helm-bibtex-bibliography "~/Cloud/Org/Bibliography/bibliography.bib")
+;;   (setq helm-bibtex-library-path "~/Cloud/Research/")
+;; 
+;;   (setq helm-bibtex-pdf-open-function 'org-open-file)
+;; 
+;;   (setq helm-bibtex-notes-path "~/Cloud/Org/Bibliography/helm-bibtex-notes"))
