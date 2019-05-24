f706cbb58495b35f4bd51641fca8d458adeaf77d
Bor Hodošček <dev@bor.space>
2019-05-24 12:40:39 +0900

    theme improvements

--- emacs.d/bor/theme-and-fonts-init.el
+++ emacs.d/bor/theme-and-fonts-init.el
@@ -1,37 +1,25 @@
-;; Load color scheme in console and GUI modes
-;; (load-theme 'sanityinc-tomorrow-night t) ;; <- For console use.
 (use-package
   doom-themes
   :config
+  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
+        doom-themes-enable-italic t) ; if nil, italics is universally disabled
   (load-theme 'doom-one t)
   (doom-themes-visual-bell-config)
   (doom-themes-org-config)
-  (doom-themes-neotree-config)
+  (doom-themes-neotree-config))
 
-  ;; (let ((line (face-attribute 'mode-line :underline)))
-  ;;   (set-face-attribute 'mode-line-inactive nil :overline   line)
-  ;;   (set-face-attribute 'mode-line          nil :overline   line)
-  ;;   (set-face-attribute 'mode-line          nil :underline  line)
-  ;;   (set-face-attribute 'mode-line          nil :box        nil)
-  ;;   (set-face-attribute 'mode-line-inactive nil :box        nil)
-  ;;   (set-face-attribute 'mode-line-inactive nil :background (doom-color 'red)))
-  )
-
-;; (use-package klere-theme
-;;   :config
-;;   (load-theme 'klere t))
-
-(use-package
-  solaire-mode
+(use-package doom-modeline
+  :ensure t
+  :hook (after-init . doom-modeline-mode)
   :config
-  ;; (add-hook 'after-change-major-mode-hook #'turn-on-solaire-mode)
-  (add-hook 'after-revert-hook #'turn-on-solaire-mode)
-  (add-hook 'ediff-prepare-buffer-hook #'solaire-mode)
-  (add-hook 'minibuffer-setup-hook #'solaire-mode-in-minibuffer)
-  (solaire-mode-swap-bg))
+  (setq doom-modeline-height 25))
 
 (use-package ;; Run M-x all-the-icons-install-fonts
   all-the-icons)
+
+;; TODO/FIXME need a way to apply this for emacsclient
+;; Below is example hook that should be able to do this:
+;; (add-hook 'after-make-frame-functions (lambda (frame) (set-fontset-font t '(#Xe100 . #Xe16f) "Fira Code Symbol")))
 
 (setq locale-coding-system 'utf-8)
 (set-terminal-coding-system 'utf-8)
@@ -51,6 +39,7 @@
     (set-fontset-font fsn 'katakana-jisx0201 jp-fontspec)
     (set-fontset-font fsn '(#x0080 . #x024F) fontspec)
     (set-fontset-font fsn '(#x0370 . #x03FF) fontspec)
+    (set-fontset-font fsn '(#xe100 . #xe16f) "Iosevka")
 
     (dolist (elt '(((concat
                      ".*"
@@ -69,39 +58,11 @@
     (progn
       (scroll-bar-mode -1)
 
-
       ;; (make-cjk-fontset "fixed" "Fira Code" "Hiragino Kaku Gothic Pro" 12 0.8)
-      (make-cjk-fontset "fixed" "Sarasa Mono T J" "Sarasa Mono T J" 12 1.0)
+      (make-cjk-fontset "fixed" "Sarasa Mono T J" "Iosevka" 11 1.0)
+      ;; (make-cjk-fontset "fixed" "Fira Code" "Sarasa Mono T J" 11 1.0)
       ;; (make-cjk-fontset "fixed" "Inziu Iosevka J" "Inziu Iosevka J" 12 1.0)
-      (make-cjk-fontset "variable" "Equity Text B" "Hiragino Mincho Pro W3" 12 0.7)
-
-      ;; ;; https://gist.github.com/yoshinari-nomura/3465571
-      ;; ;; フォントセットを作る
-      ;; (let* ((fontset-name "myfonts") ; フォントセットの名前
-      ;;        (size 14) ; ASCIIフォントのサイズ [9/10/12/14/15/17/19/20/...]
-      ;;
-      ;;        ;; ASCIIフォント
-      ;;        ;;(asciifont "Ricty Discord")
-      ;;        ;;(asciifont "Fantasque Sans Mono")
-      ;;        (asciifont "Fira Code")
-      ;;        (asciifont-proportional "Equity Text B")
-      ;;
-      ;;        ;; 日本語フォント
-      ;;        (jpfont "Hiragino Kaku Gothic Pro")
-      ;;        (jpfont-proportional "Hiragino Mincho Pro W3")
-      ;;        ;;(jpfont "Ricty Discord")
-      ;;
-      ;;        (font (format "%s-%d:weight=normal:slant=normal" asciifont size))
-      ;;        (font-proportional (format "%s-%d:weight=normal:slant=normal" asciifont size))
-      ;;        (fontspec (font-spec :family asciifont))
-      ;;        (jp-fontspec (font-spec :family jpfont))
-      ;;        (fsn (create-fontset-from-ascii-font font nil fontset-name)))
-      ;;   (set-fontset-font fsn 'japanese-jisx0213.2004-1 jp-fontspec)
-      ;;   (set-fontset-font fsn 'japanese-jisx0213-2 jp-fontspec)
-      ;;   (set-fontset-font fsn 'katakana-jisx0201 jp-fontspec) ; 半角カナ
-      ;;   (set-fontset-font fsn '(#x0080 . #x024F) fontspec) ; 分音符付きラテン
-      ;;   (set-fontset-font fsn '(#x0370 . #x03FF) fontspec) ; ギリシャ文字
-      ;;   )
+      (make-cjk-fontset "variable" "Equity Text B" "Sarasa UI J" 11 1.0)
 
       ;; デフォルトのフレームパラメータでフォントセットを指定
       (add-to-list 'default-frame-alist '(font . "fontset-fixed"))
@@ -160,5 +121,9 @@
   '((t (:overline "#003f8e" :foreground "#ffffff" :background "#002451")))
   "Face used for the line delimiting the end of source blocks.")
 
-(use-package centered-window :ensure t
-  :config (centered-window-mode t))
+;; (use-package centered-window :ensure t
+;;   :config (centered-window-mode t))
+
+(require 'ligatures-init)
+
+;; (require 'fira-code-mode)
