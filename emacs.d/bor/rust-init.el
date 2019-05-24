158d27ca2ebccda6b9ed661c34ae86dba783ecf0
Bor Hodošček <dev@bor.space>
2019-05-24 12:40:59 +0900

    rust setup improvements

--- emacs.d/bor/rust-init.el
+++ emacs.d/bor/rust-init.el
@@ -1,9 +1,9 @@
-(use-package
-  cargo
+(use-package rust-mode)
+(use-package flymake-rust)
+(use-package racer)
+(use-package cargo
   :config
   (add-hook 'rust-mode-hook 'cargo-minor-mode))
-
-(use-package
-  flycheck-rust
+(use-package flycheck-rust
   :config
   (add-hook 'flycheck-mode-hook #'flycheck-rust-setup))
